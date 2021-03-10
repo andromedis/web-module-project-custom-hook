f57d5c6383b25f9cd6492ea7a304c738d8f4b482
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-10 20:41:47 +0000

    useDarkMode hook added to App, controls darkMode state. localStorage shows darkMode value. MVP complete

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,8 @@
 import useLocalStorage from './useLocalStorage'
 
-const useDarkMode = () => {
-    const [isDark, setIsDark] = useLocalStorage('useDark');
-    return [isDark, setIsDark];
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode', initialValue);
+    return [darkMode, setDarkMode];
 }
+
+export default useDarkMode;
